037b2ec0ff20a283e41e014ddf301ba9d5250596
Salinas04 <antoniosalinamoreno@gmail.com>
2025-07-23 17:48:21 +0200

    update API base URL in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: 'vercel'
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/motion/nuxt'
